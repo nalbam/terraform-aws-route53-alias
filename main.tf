a890a151f230313d9999ece8de93809e81aa180a
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-05-29 13:21:26 +0900

    terraform 0.12upgrade

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_route53_record" "default" {
-  zone_id = "${var.zone_id}"
+  zone_id = var.zone_id
 
-  name = "${var.name}"
+  name = var.name
   type = "A"
 
   alias {
-    name = "${var.alias_name}"
-    zone_id = "${var.alias_zone_id}"
+    name                   = var.alias_name
+    zone_id                = var.alias_zone_id
     evaluate_target_health = "false"
   }
 }
